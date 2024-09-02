423f95132ad8e55bfe709223ebac142d24c43592
Utpal <dev.theutpal@gmail.com>
2024-09-02 17:11:59 +0530

    FIX: Bugs

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,7 +1,16 @@
+import Spline from "@splinetool/react-spline";
 import React from "react";
 
 function Projects() {
-	return <div id="projects" className="min-h-screen flex justify-center  items-center font-display">Coming Soon!</div>;
+	return (
+		<div
+			id="projects"
+			className="min-h-screen flex justify-center  items-center font-display"
+		>
+			{/* <Spline scene="https://prod.spline.design/ea9r5QWig9m7LwIh/scene.splinecode" /> */}
+			Coming Soon...
+		</div>
+	);
 }
 
 export default Projects;
